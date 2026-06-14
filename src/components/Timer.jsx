15ebb4668f6123db679cfab8cf77b0e31dc94108
Akzeno <shyam246820@gmail.com>
2026-06-14 20:23:07 +0530

    add break and workMode logic

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -3,46 +3,74 @@ import ShowTime from "./ShowTime";
 import ShowTimeWraper from "./ShowTimeWraper";
 
 const Timer = () => {
-  const [time, setTime] = useState(300);
+  const timeGivenbyUser = {
+    usrTimeInSec: 12,
+    usrBeakTimeInSec: 5,
+  };
+
+  const [time, setTime] = useState(timeGivenbyUser.usrTimeInSec);
   const [runnig, setRunning] = useState(false);
+  const [isbreakTime, setIsBreakTime] = useState(false);
+  
 
   useEffect(() => {
-    if (time <= 0) return;
-    if (!runnig) return;
+    if (time <= 0 || !runnig) return;
+
     const intervelId = setInterval(() => {
       setTime((prevTime) => {
-        if (prevTime <= 1) {
-          clearInterval(intervelId);
-          return 0;
-        }
         return prevTime - 1;
       });
     }, 1000);
     return () => clearInterval(intervelId);
   }, [runnig]);
 
+  useEffect(() => {
+    if (time === 0) {
+      setRunning(false);
+
+      let nextIsBreak;
+
+      setIsBreakTime((prevStat) => {
+        nextIsBreak = !prevStat;
+        setTime(
+          nextIsBreak
+            ? timeGivenbyUser.usrBeakTimeInSec
+            : timeGivenbyUser.usrTimeInSec,
+        );
+        return nextIsBreak;
+      });
+    }
+  }, [time, timeGivenbyUser.usrBeakTimeInSec, timeGivenbyUser.usrTimeInSec]);
+
   function handelReset() {
     setRunning(false);
-    setTime(300);
+    setTime(timeGivenbyUser.usrTimeInSec);
   }
 
-  const hour = Math.floor(time / 3600);
-  const minuts = Math.floor((time % 3600) / 60);
-  const seconds = time % 60;
+  const splitedTime = {
+    hour: Math.floor(time / 3600),
+    minuts: Math.floor((time % 3600) / 60),
+    seconds: time % 60,
+  };
 
   return (
     <div className="flex h-screen items-center justify-center bg-zen-ink font-['Rubik'] text-[18vw] leading-none font-extrabold tracking-tighter text-neutral-600 select-none">
-      <ShowTimeWraper setRunning={setRunning} runnig={runnig} handelReset={handelReset}>
+      <ShowTimeWraper
+        setRunning={setRunning}
+        runnig={runnig}
+        handelReset={handelReset}
+        isbreakTime={isbreakTime}
+      >
         {time >= 3600 && (
           <>
-            <ShowTime time={hour} />
+            <ShowTime time={splitedTime.hour} />
             <span className="relative bottom-[1vw]">:</span>
           </>
         )}
 
-        <ShowTime time={minuts} />
+        <ShowTime time={splitedTime.minuts} />
         <span className="relative bottom-[1vw]">:</span>
-        <ShowTime time={seconds} />
+        <ShowTime time={splitedTime.seconds} />
       </ShowTimeWraper>
     </div>
   );
